1ed77f7dbbca3c03b0a96a75c3afe7f3d824ed90
mlopez2231 <mlopez2231@ceuandalucia.es>
2024-10-25 16:31:27 +0200

    ManuelLopez
    Proyecto3_3

---
+++ Proyecto3_3/src/main/java/Practica3_3.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Practica3_3 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+
+	System.out.println("Manuel López Tovar");
+	
+	}
+}
